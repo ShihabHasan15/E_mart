a72ed897b59f025512d97790d57cfb049edacdd1
Md Shihab Hasan <shihabhasanhridoy06@gmail.com>
2024-04-22 01:12:48 +0600

    commit

---
+++ app/src/main/java/com/shibustudio/emart/Account_activity.java
@@ -0,0 +1,14 @@
+package com.shibustudio.emart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Account_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
